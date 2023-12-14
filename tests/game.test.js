f7641185b1646db3799489aba4527effa79a1de7
alexwittwer <wittweral@gmail.com>
2023-12-14 17:55:57 -0500

    add game todos

---
+++ tests/game.test.js
@@ -0,0 +1,7 @@
+import { describe, it, test, expect } from "vitest";
+
+describe.todo("game initializes");
+describe.todo("p1 turn starts at game start");
+describe.todo("players swap turns");
+describe.todo("game ends if a players ships are all sunk");
+describe.todo("winner is announced");
