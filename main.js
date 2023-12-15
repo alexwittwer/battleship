826d9d654a7698d3e31b01c4109ba783022d836d
alexwittwer <wittweral@gmail.com>
2023-12-15 17:41:19 -0500

    testing

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 import "./style.css";
+import Player from "./modules/player";
+
+const p1 = new Player("tom", true);
+const p2 = new Player("betty");
+
+p2.gameboard.placeShipH(p2.carrier, [0, 0]);
+p2.gameboard.placeShipH(p2.cruiser, [1, 0]);
+p2.gameboard.placeShipH(p2.destroyer, [2, 0]);
+p2.gameboard.placeShipH(p2.submarine, [3, 0]);
+p2.gameboard.placeShipH(p2.pboat, [4, 0]);
+
+console.log(p2.gameboard.board);
